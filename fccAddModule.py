c574a6cd7ae13324094fd86cc0288964b7c2cc50
John Wilkins <jowilkin@redhat.com>
2020-09-04 16:59:04 -0700

    Added a module rename option. Removed the old -s source for topic conversion as it was legacy code.
    Signed-off-by: John Wilkins <jowilkin@redhat.com>

--- fccAddModule.py
+++ fccAddModule.py
@@ -2,7 +2,9 @@
 
 # This program creates module files in the flexible content format.
 # It creates the file, file name, module ID, heading anchor and heading.
-# The program must run in the same directory as the master.adoc and assembly files.
+# The program must run in the same directory as the master.adoc and assembly
+# files. If you specify the -o option, it will rename the module, include the
+# assembly include directive if -a is specified.
 
 import os
 import sys
@@ -16,73 +18,105 @@ def main(argv):
     configParser.read('./fccAddModule.conf')
     moduleType = configParser.get('fcc-add-module-conf', 'defaultModuleType')
     moduleDestinationPath = configParser.get('fcc-add-module-conf', 'moduleDestinationPath')
-    moduleSourcePath = configParser.get('fcc-add-module-conf', 'moduleSourcePath')
-    moduleSourceFileName = ""
     componentType = configParser.get('fcc-add-module-conf', 'defaultComponentType')
     locale = configParser.get('fcc-add-module-conf', 'defaultLocale')
     fileExtension = configParser.get('fcc-add-module-conf', 'fileExtension')
+
+    #Initialize variables
     ctx = "_{context}"
     assemblyFileName = ""
+    oldName = ""
+    moduleName = ""
 
     try:
-        moduleName = ""
 
         #define command line arguments.
-        opts, args = getopt.getopt(argv,"n:t:c:a:s:d:h",["name=","type=","component=","assembly=","source=", "destination=", "help"])
+        opts, args = getopt.getopt(argv,"n:o:t:c:a:d:h",["name=","old=","type=","component=","assembly=", "destination=", "help"])
 
         for opt, arg in opts:
             if opt in ("-n", "--name"):
                 moduleName = arg
+            elif opt in ("-o", "--old"):
+                oldName = arg
             elif opt in ("-t", "--type"):
                 moduleType = arg
             elif opt in ("-c", "--component"):
                 componentType = arg
             elif opt in ("-a", "--assembly"):
                 assemblyFileName = arg
-            elif opt in ("-s", "--source"):
-                moduleSourceFileName = arg
             elif opt in ("-d", "--destination"):
                 moduleDestinationPath = arg
             elif opt in ("-h", "--help"):
                 printUsage()
                 sys.exit()
 
+        #fccAddModule ALWAYS requires a module name.
         if moduleName == "":
             print ("\nERROR: Module name is required! Use the -n or --name option.\n")
             printUsage()
             sys.exit()
 
-        lowercaseModuleName = moduleName.lower()
-        baseModuleId = lowercaseModuleName.replace(' ', '-')
+        #Create a heading comment for the file header.
+        headingComment = getHeadingComment(assemblyFileName)
+
+        #The outputModuleId is a component of the outputFileName and moduleID.
+        outputModuleId = getModuleID(moduleName)
+
+        moduleId = "[id='" + outputModuleId + ctx + "']"
+
+        outputFileName = createFileName(moduleDestinationPath, moduleType, componentType, outputModuleId, locale, fileExtension)
+
+        includeText = getIncludeDirective(outputFileName)
+
+        #Test if oldName exists, and rename the module if it does.
+        if oldName !="":
+            # The inputModuleId is derived from the old name and to parse get inputFileName and old moduleID during rename operations.
+            inputModuleId = getModuleID(oldName)
+
+            inputFileName = createFileName(moduleDestinationPath, moduleType, componentType, inputModuleId, locale, fileExtension)
+
+            inputFile = open (inputFileName, 'r')
+            moduleData = inputFile.read()
+            inputFile.close()
+
+            moduleMod = moduleData.replace(inputModuleId, outputModuleId)
+            moduleData = moduleMod.replace(oldName, moduleName)
+
+            outFile = open(outputFileName, 'w') #output file to write to.
+            outFile.write(moduleData)
+            outFile.close()
+
+            os.remove(inputFileName)
+            print ("\n- Renamed module from" + oldName + "\nto:\n" + moduleName)
+
+            if assemblyFileName != "":
+                assemblyFile = open(assemblyFileName, 'r')
+                assemblyData = assemblyFile.read()
+                assemblyFile.close()
+
+                inputIncludeText = getIncludeDirective(inputFileName)
+                modifiedAssemblyData = assemblyData.replace(inputIncludeText, includeText)
+
+                assemblyFile = open(assemblyFileName, 'w')
+                assemblyFile.write(modifiedAssemblyData)
+                assemblyFile.close()
+            print ("\n- Renamed include text")
+
+        else:
+            outFile = open(outputFileName, 'w') #output file to write to.
+            outFile.write(headingComment + "\n\n")
+            outFile.write(moduleId + "\n\n")
+            outFile.write("= " + moduleName + "\n")
+            print ("\n- Added module heading.\n- Added module ID")
+
+            if assemblyFileName != "":
+                assemblyFile = open(assemblyFileName, 'a')
+                assemblyFile.write("\n" + includeText + "\n")
+                print("- Added include to assembly file.")
+
+            print ("Output File: " + outputFileName)
 
 
-        outputFileName = moduleDestinationPath + moduleType + "_" + componentType + "_" + baseModuleId + "_" + locale + fileExtension
-        headingComment = "// This is included in the following assemblies:\n//\n// " + assemblyFileName
-        moduleId = "[id='" + baseModuleId + ctx + "']"
-
-        if moduleSourceFileName != "":
-            sourceFile = open(moduleSourcePath + moduleSourceFileName, 'r')
-            sourceFile.next()
-
-        outFile = open(outputFileName, 'w') #output file to write to.
-        outFile.write(headingComment + "\n\n")
-        outFile.write(moduleId + "\n\n")
-        outFile.write("= " + moduleName + "\n")
-        print ("\n- Added module ID\n- Added module heading.")
-
-        if moduleSourceFileName != "":
-            for line in sourceFile:
-                outFile.write(line)
-            print("- Added module source")
-
-        includeText = "include::{includedir}/" + outputFileName + "[leveloffset=+1]"
-
-        if assemblyFileName != "":
-            assemblyFile = open(assemblyFileName, 'a')
-            assemblyFile.write("\n" + includeText + "\n")
-            print("- Added include to assembly file.")
-
-        print ("Output File: " + outputFileName)
     except OSError as e:
         print(str(e))
 
@@ -91,17 +125,35 @@ def main(argv):
         sys.exit(2)
 
 
+#Takes a title case string name of a module, and coverts it to a
+#lowercase dash(-) delimited module ID for anchor tags and file names.
+def getModuleID(name):
+    lowercaseModuleName = name.lower()
+    return lowercaseModuleName.replace(' ', '-')
+
+#Takes destination path, module type, component type, module ID, locale and extension and creates a file name.
+def createFileName(dpath, mtype, ctype, modId, locale, ext):
+    return dpath + mtype + "_" + ctype + "_" + modId + "_" + locale + ext
+
+#Takes an assembly file name and gets a heading comment.
+def getHeadingComment(assemblyFileName):
+    return "// This is included in the following assemblies:\n//\n// " + assemblyFileName
+
+#Takes a file name and returns an include directive for the assembly file.
+def getIncludeDirective(fileName):
+        return "include::{includedir}/" + fileName + "[leveloffset=+1]"
+
 
 
 def printUsage():
         print "\n\n====================\nfccAddModule.py Help\n====================\n\n\tThe fccAddModule.py helper program will create a new module in the flexible customer\n\tcontent/modular format. This helper program MUST run in the same directory as the\n\tmaster.adoc file. To incorporate existing topic content, use the -s or --source option.\n\tNOTE: The -s or --source will omit the first line to erase the existing [[anchor-tag]].\n\tTo APPEND an include statement to an assembly, use the -a or --assembly option.\n\tThe assembly file SHOULD exist already; however, you may create a file without\n\tthe required formatting on-the-fly. To override the default component type in\n\tfccAddModule.conf, use the -c or --component option."
         print "\nUSAGE: \n\t$ python fccAddModule.py -n '<moduleName>' [options] \n\nOPTIONS:\n"
         print "\t-n '<moduleName>' OR --name '<moduleName>' REQUIRED"
+        print "\t-o '<oldModuleName>' OR --old '<oldModuleName>' OPTIONAL"
         print "\t-t (proc|con|ref|assembly) OR --type (proc|con|ref|assembly) OPTIONAL. DEFAULT = proc"
         print "\t-a <assemblyFile> OR --assembly <assemblyFile> OPTIONAL"
-        print "\t-c <componentName> OR --component <componentName>"
-        print "\t-s <sourceFileName> OR --source <sourceFileName> The source file to include. OPTIONAL"
-        print "\t-d <moduleDestinationPath> OR --destination <moduleDestinationPath> The destination path for the module. OPTIONAL\n"
+        print "\t-c <componentName> OR --component <componentName> OPTIONAL. For default see fccAddModule.conf."
+        print "\t-d <moduleDestinationPath> OR --destination <moduleDestinationPath> The destination path for the module. OPTIONAL. DEFAULT = modules/.\n"
 
 if __name__ == "__main__":
     main(sys.argv[1:])
