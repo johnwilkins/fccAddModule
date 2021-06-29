0aefdd4b6d2ee59a1d0f40389c620407a66db19b
John Wilkins <jowilkin@redhat.com>
2021-06-29 09:38:51 -0700

    Compliance with FCC.
    Signed-off-by: John Wilkins <jowilkin@redhat.com>

--- addModule.py
+++ addModule.py
@@ -66,7 +66,7 @@ def main(argv):
         #The outputModuleId is a component of the outputFileName and moduleID.
         outputModuleId = getModuleID(moduleName)
 
-        moduleId = "[id='" + outputModuleId + ctx + "']"
+        moduleId = "[id=\"" + outputModuleId + ctx + "\"]"
 
         if fileNameFormat == "fcc":
             outputFileName = createFccFileName(moduleDestinationPath, moduleType, componentType, outputModuleId, locale, fileExtension)
